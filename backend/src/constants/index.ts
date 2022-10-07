ee0d9aa306d0bf3504ec1649edb70b88125c77d8
Shrey Rohatgi <shreyrohatgi@Shreys-MacBook-Air.local>
2022-10-07 16:11:58 +0530

    🎉 fronten

--- backend/src/constants/index.ts
+++
@@ -1,8 +0,0 @@
-import dotenv from "dotenv";
-dotenv.config();
-
-export const port = process.env.PORT || 7000;
-export const db_url = process.env.DB_URL || "";
-export const sid = process.env.SID || "";
-export const auth_token = process.env.AUTH_TOKEN || "";
-export const sender_number = process.env.SENDER_NUMBER || "";
--- backend/src/interface/SMS.ts
+++
@@ -1,7 +0,0 @@
-export interface ISMS {
-  firstName: string;
-  lastName: string;
-  time: Date;
-  otp: string;
-  receiverNumber: string;
-}
